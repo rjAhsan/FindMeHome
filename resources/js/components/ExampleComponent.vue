765df579c7bb6341e6daad96deaa45d87abc1068
rjahsan43@gmail.com <rjahsan43@gmail.com>
2019-09-07 19:28:52 +0500

    add fresh project

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,26 @@
+<template>
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body">
+                        <div>
+                            <h1>Laravel New Version 6 Has been Realesed</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted reated .')
+        }
+    }
+</script>
